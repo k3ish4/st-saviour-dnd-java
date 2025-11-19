25a9c8f2ac6aca15e9e089030d9581a3360d509f
Keisha <k3ish4.v@gmail.com>
2025-11-18 23:34:04 -0500

    quick time event done

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -110,41 +110,81 @@ public class Game {
     } else if (choice1.equals("b") || choice1.equals("B")){
         System.out.println("You mind your business and hurry off to class.");
     } else if (choice1.equals("c") || choice1.equals("C")){
-        System.out.println("Everyone sees you attemt to ridicule this girl and now they think you're a jerk. \n -2 Aura");
-        aura -= 1;
+        System.out.println("Everyone sees you attempt to ridicule this girl and now they think you're a jerk. \n -2 Aura");
+        aura -= 2;
     }  
     if (aura <= 0){
-        System.out.println("You've lost all your Aura and failed to survive a day at St. Saviour. Game Over.");
+        System.out.println("You've taken too much damage and failed to survive a day at St. Saviour. Game over.");
     } else{
         System.out.println("During your math class, Mr. Sadushi calls on you to answer a question when you weren't paying attention.")
         System.out.println("Will you...\n A. Take a wild guess\n B. Say nothing\n C. Attempt to get out of this by cracking a joke")
         String choice2 = scanner.nextLine();
-    if (choice2.equals("a") || choice2.equals("A")){
-        int roll = Player.rollD6();
-        if (intelligence >= 3){
-            roll += 1;
-            System.out.println("Your intelligence may give you a boost!");
-        }
-        if (roll > 3){
-            System.out.println("You guessed correctly and avoided aura damage!\n +1 Experience");
-            experience += 1;
-        } else {
-            System.out.println("You unfortunately guessed really wrong. Someone in the back of the class laughed. At least you tried!\n -1 Aura\n +1 Experience");
+        if (choice2.equals("a") || choice2.equals("A")){
+            int roll = Player.rollD6();
+            if (intelligence > 3){
+                roll += 1;
+                System.out.println("Your intelligence may give you a boost!");
+            }
+            if (roll > 3){
+                System.out.println("You guessed correctly and avoided aura damage!\n +1 Experience");
+                experience += 1;
+            } else {
+                System.out.println("You unfortunately guessed really wrong. Someone in the back of the class laughed. At least you tried!\n -1 Aura\n +1 Experience");
+                experience += 1;
+                aura -= 1;
+            }
+    }   else if (choice2.equals("b") || choice2.equals("B")){
+            System.out.println("After some awkward silence, Mr. Sadushi lets you off the hook and chooses someone else.\n -1 Aura\n -1 Experience");
+            experience -= 1;
             aura -= 1;
-        }
-    } else if (choice2.equals("b") || choice2.equals("B")){
-        System.out.println("After some awkward silence, Mr. Sadushi lets you off the hook and chooses someone else.\n -1 Aura\n -1 Experience");
-    } else if (choice2.equals("c") || choice2.equals("C")){
-        int roll = Player.rollD6();
-        if (experience > 3){
-            roll += 1;
-            System.out.println("Your Experience may give you a boost!");
-        }
-        if (roll > 3){
-            System.out.println("Everyone laughed it off and moved on. You avoided aura damage!");
+    }   else if (choice2.equals("c") || choice2.equals("C")){
+            int roll = Player.rollD6();
+            if (experience > 3){
+                roll += 1;
+                System.out.println("Your Experience may give you a boost!");
+            }
+            if (roll > 3){
+                System.out.println("Everyone laughed it off and moved on. You avoided aura damage!");
+            } else {
+                System.out.println("Mr. Sadushi was not amused by your joke.\n -1 Aura");
+                aura -= 1;
+        if((aura <= 0) || (experience <= 0)){
+            System.out.println("You've taken too much damage and failed to survive a day at St. Saviour. Game over.");
         } else {
-            System.out.println("Mr. Sadushi was not amused by your joke.\n -1 Aura");
-    }
+           System.out.println("Your next class is Gym, so you make your way downstairs.");
+           System.out.println("!!!QUICK TIME EVENT!!!\n You feel yourself lose your footing for a moment on the stairs! \n Type the word 'save' within 5 sec to save yourself from falling!");
+           String qt1 = TimedInput.getUserInputWithTimeout(10);
+           if (qt1.equals("save")){
+            System.out.println("Phew! that was close!")
+           }else{
+            System.out.println("You fell down the stairs pretty badly and like 5 people saw. You twisted your ankle and embarrassed yourself. \n -1 Aura \n -1 Agility");
+            agility -= 1;
+            aura -= 1;
+           }
+           if((aura <= 0) || (agility <= 0)){
+            System.out.println("You've taken too much damage and failed to survive a day at St. Saviour. Game over.");
+           }else{
+            System.out.println("\nDuring a game of kickball at Gym class, you're standing at first base and you see an opportunity to run all the way back to home base without being intercepted.\nYou also notice at the last minute that your shoelace is untied and you have no time to sort that out.")
+            System.out.println("Will you...\n A. Run all the way to home base at the risk of tripping on your shoelace\n B. Play it safe and just run to second base.");
+            String choice1 = scanner.nextLine();
+            if (choice1.equals("a") || choice1.equals("A")){
+                int roll = Player.rollD6();
+                if (agility > 3){
+                    roll += 1;
+                    System.out.println("Your Agility may give you a boost!");
+                }
+                if (roll > 3){
+                    System.out.println("You made it to home base without tripping and just earned your team the winning point!\n +1 Experience");
+                    Experience += 1;
+                } else {
+                    System.out.println("You tripped on your shoelace and fell flat on your face. You managed to walk it off with no aura damage whilst hiding the fact that you nose is killing you. \n -1 Agility");
+                    agility -= 1;
+                } else if (choice1.equals("b") || choice1.equals("B")){
+                    System.out.println("You decided not to try your luck and take things one base at a time.");
+                }
+           
+            }     
+        }
     
     //String answer = TimedInput.getUserInputWithTimeout(10);
     //System.out.println("You entered " + answer + " in time!");
